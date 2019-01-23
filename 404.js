a2f7837f30d988a858928bcbf305d43933e53d89
nelvson <nelvson1337@gmail.com>
2019-01-23 23:39:17 +0700

    Add 404 page. TODO: find a way to randomise 404 message, find a theme

--- 404.js
+++ 404.js
@@ -1,6 +1,6 @@
 let root = document.getElementById('root');
 const PASSING_STUFF =
-  'https://raw.githubusercontent.com/nelvson/nelvson.github.io/master/passingStuff.json';
+  'https://raw.githubusercontent.com/nelvson/nelvson.github.io/master/messages.json';
 //root.innerHTML = 'hello there general kenobi';
 fetch(PASSING_STUFF)
   .then((result) => result.json())
@@ -10,6 +10,6 @@ fetch(PASSING_STUFF)
   .catch((e) => console.log('Error happened!', e));
 
 let renderPage = (data) => {
-  document.title = data[0].title;
-  root.innerHTML = data[0].body;
+  root.innerHTML = data[0].message;
+  //TODO : find a way to pick random message
 };
