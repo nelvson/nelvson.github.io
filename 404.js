2ef6be82c8aa67075d8cf22f1fc7a7a19ec7a45b
nelvson <nelvson1337@gmail.com>
2019-01-24 10:11:17 +0700

    Collide messages.json to passingStuff.json. TODO: Find a better name for passingStuff.json

--- 404.js
+++ 404.js
@@ -1,6 +1,8 @@
+//TODO : error on re-declaration let root
+const PAGE_TYPE = 404;
 let root = document.getElementById('root');
 const PASSING_STUFF =
-  'https://raw.githubusercontent.com/nelvson/nelvson.github.io/master/messages.json';
+  'https://raw.githubusercontent.com/nelvson/nelvson.github.io/master/passingStuff.json';
 //root.innerHTML = 'hello there general kenobi';
 fetch(PASSING_STUFF)
   .then((result) => result.json())
@@ -10,6 +12,11 @@ fetch(PASSING_STUFF)
   .catch((e) => console.log('Error happened!', e));
 
 let renderPage = (data) => {
-  root.innerHTML = data[0].message;
+  let page = [];
+  console.log(data);
+  for (d of data) {
+    if (data.type === PAGE_TYPE) page.push(data);
+  }
+  console.log(page);
   //TODO : find a way to pick random message
 };
